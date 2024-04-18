44d9813e80f9c7d3cb0e6487d99e47cfa227bad2
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2024-04-18 03:05:17 +0200

    feat: publish config
    Signed-off-by: Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>

--- libs/core/src/lib/core.ts
+++ libs/core/src/lib/core.ts
@@ -1,3 +1,51 @@
-export function core(): string {
-  return 'core';
-}
+import { EventEmitter } from 'eventemitter3';
+
+export const emitter = new EventEmitter();
+
+type Subscribe = (
+  event: string,
+  callback: (data: unknown) => void,
+) => {
+  data: unknown;
+  unsubscribe: () => void;
+};
+
+export const subscribe: Subscribe = (event, callback) => {
+  let data;
+
+  const _callback = (_data: unknown) => {
+    data = _data;
+    callback(data);
+  };
+
+  console.log(`Subscribed to event '${event}'`);
+
+  emitter.on(event, _callback);
+
+  // console.table({
+  //   event: emitter.eventNames(),
+  //   listeners: emitter.listenerCount(event),
+  // });
+
+  const unsubscribe = () => {
+    emitter.off(event, _callback);
+    console.log(`Unsubscribed from event '${event}'`);
+  };
+
+  return { data, unsubscribe };
+};
+
+export const subscribeOnce = (
+  event: string,
+  callback: (data: unknown) => void,
+) => {
+  emitter.once(event, callback);
+  return { unsubscribe: () => emitter.off(event, callback) };
+};
+
+type Publish = (event: string, data?: unknown) => void;
+
+export const publish: Publish = (event, data) => {
+  emitter.emit(event, data);
+  console.log(`Published '${event}' event with data: ${JSON.stringify(data)}`);
+};
