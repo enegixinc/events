130fe6ad00e66123ff96dc0f9de17b90fed59757
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2024-06-03 00:36:49 +0300

    stash
    Signed-off-by: Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>

--- examples/react-example/src/app/app.tsx
+++ examples/react-example/src/app/app.tsx
@@ -1,13 +1,58 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
+import React from 'react';
+import * as go from 'gojs';
+import { ReactDiagram } from 'gojs-react';
 
-import NxWelcome from './nx-welcome';
+function initDiagram() {
+  // set your license key here before creating the diagram: go.Diagram.licenseKey = "...";
+  const diagram = new go.Diagram({
+    'undoManager.isEnabled': true, // must be set to allow for model change listening
+    // 'undoManager.maxHistoryLength': 0,  // uncomment disable undo/redo functionality
+    'clickCreatingTool.archetypeNodeData': {
+      text: 'new node',
+      color: 'lightblue',
+    },
+    model: new go.GraphLinksModel({
+      linkKeyProperty: 'key', // IMPORTANT! must be defined for merges and data sync when using GraphLinksModel
+    }),
+  });
+
+  // define a simple Node template
+  diagram.nodeTemplate = new go.Node('Auto') // the Shape will go around the TextBlock
+    .bindTwoWay('location', 'loc', go.Point.parse, go.Point.stringify)
+    .add(
+      new go.Shape('RoundedRectangle', {
+        name: 'SHAPE',
+        fill: 'white',
+        strokeWidth: 0,
+      })
+        // Shape.fill is bound to Node.data.color
+        .bind('fill', 'color'),
+      new go.TextBlock({ margin: 8, editable: true }) // some room around the text
+        .bindTwoWay('text')
+    );
+
+  return diagram;
+}
 
 export function App() {
   return (
-    <div>
-      <NxWelcome title="react-example" />
-    </div>
+    <ReactDiagram
+      initDiagram={initDiagram}
+      divClassName="diagram-component"
+      nodeDataArray={[
+        { key: 0, text: 'Alpha', color: 'lightblue', loc: '0 0' },
+        { key: 1, text: 'Beta', color: 'orange', loc: '150 0' },
+        { key: 2, text: 'Gamma', color: 'lightgreen', loc: '0 150' },
+        { key: 3, text: 'Delta', color: 'pink', loc: '150 150' },
+      ]}
+      linkDataArray={[
+        { key: -1, from: 0, to: 1 },
+        { key: -2, from: 0, to: 2 },
+        { key: -3, from: 1, to: 1 },
+        { key: -4, from: 2, to: 3 },
+        { key: -5, from: 3, to: 0 },
+      ]}
+    />
   );
 }
 
