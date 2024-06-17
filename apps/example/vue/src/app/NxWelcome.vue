af04180e384473e365f20bd120e0ad6e0ad21a87
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2024-06-17 17:02:58 +0300

    feat: base error
    Signed-off-by: Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>

--- apps/example/vue/src/app/NxWelcome.vue
+++ apps/example/vue/src/app/NxWelcome.vue
@@ -10,10 +10,20 @@ subscribe('subscribe', (data) => {
 });
 
 publish('publish', { title: 'asd' });
+
+const handleClick = () => {
+  subscribe('subscribe', (data) => {
+    console.log(data);
+  });
+  publish('publish', { title: 'vue' });
+
+  const error = new Error();
+  console.log(error.stack);
+};
 </script>
 
 <template>
   <div class="wrapper">
-    <button @click="">publish</button>
+    <button @click="handleClick">publish</button>
   </div>
 </template>
